fcaca99fa316bc56a9162479699928037a143f56
Ben Kempers <77750841+bkempers@users.noreply.github.com>
2026-03-15 18:06:19 -0600

    chore: fix git ignorecase directory name (#5)
    * fix git merge case ignore directory name
    
    * fix rtos dir filename in ceedling yml

--- RTOS/kernel.c
+++
@@ -1,8 +0,0 @@
-#include "include/kernel.h"
-#include "config.h"
-
-struct tcb tcbs[NUM_THREADS];
-struct tcb *current_ptr;
-
-// each thread will have a stack size of 100 i.e. 400 bytes
-int32_t TCB_STACK[NUM_THREADS][STACK_SIZE];
